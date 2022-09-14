72862d191bed5b6c6c652fe1debc5d3cd188810e
Yuri Braga <yuri.moc.rb@gmail.com>
2022-09-14 13:34:34 -0300

    feat: change to HomeScreen

--- App.js
+++ App.js
@@ -1,75 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
-import { db } from './firebase';
-import { useEffect, useState } from 'react';
-import { onValue, ref, set } from '@firebase/database';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [message, setMessage] = useState('')
-  const [messages, setMessages] = useState([])
-
-  useEffect(() => {
-    onValue(ref(db, 'chat'), snapshot => {
-      const data = snapshot.val()
-
-
-      if (data) {
-        Object.values(data).forEach(item => {
-          console.log('item', item)
-          setMessages(oldItems => [...oldItems, item])
-        })
-      }
-
-      console.log(messages)
-    })
-  }, [])
-
-  function createMessage() {
-    const time = new Date().getTime()
-
-    set(ref(db, `/chat/${time}`), {
-      'user': 'Yuri',
-      'message': message
-    })
-
-    setMessage('')
-  }
-
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <View style={styles.container}>
-          {messages.map(item => (
-            <Text style={{color: 'orange'}}>{JSON.stringify(item)}</Text>
-          ))}
-          <Text style={{color: 'orange'}}>Mensagem!</Text>
-          <TextInput style={styles.textInput} value={message} onChangeText={setMessage} />
-          <Button
-            style={styles.button}
-            color='orange'
-            title="Press me"
-            onPress={createMessage}
-          />
-          <StatusBar style="auto" />
-        </View>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} />
+        </Stack.Navigator>
+        <StatusBar style="auto" />
       </NavigationContainer>
     </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 10
-  },
-  textInput: {
-    borderBottomWidth: 1,
-    borderBottomColor: 'orange',
-    height: 24
-  }
-});
+
---
+++ screens/HomeScreen.js
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react';
+import { db } from '../firebase';
+import { onValue, ref, set } from '@firebase/database';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+
+const HomeScreen = ({ navigation }) => {
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    onValue(ref(db, 'chat'), snapshot => {
+      const data = snapshot.val()
+
+
+      if (data) {
+        Object.values(data).forEach(item => {
+          console.log('item', item)
+          setMessages(oldItems => [...oldItems, item])
+        })
+      }
+
+    })
+  }, [])
+
+  function createMessage() {
+    const time = new Date().getTime()
+
+    set(ref(db, `/chat/${time}`), {
+      'user': 'Yuri',
+      'message': message
+    })
+
+    setMessage('')
+  }
+
+  return (
+    <View style={styles.container}>
+      {messages.map((item) => (
+        <Text style={{ color: "orange" }}>{JSON.stringify(item)}</Text>
+      ))}
+      <Text style={{ color: "orange" }}>Mensagem!</Text>
+      <TextInput
+        style={styles.textInput}
+        value={message}
+        onChangeText={setMessage}
+      />
+      <Button
+        style={styles.button}
+        color="orange"
+        title="Press me"
+        onPress={createMessage}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10
+  },
+  textInput: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'orange',
+    height: 24
+  }
+});
+
+export default HomeScreen;
